0fdf9853f0fdcfedb47692b900a70619f90e643b
pete <petewicken@gmail.com>
2013-08-26 20:39:34 +0100

    removed unused import

--- FlashLight/src/main/java/com/jamobox/flashlight/FlashNotification.java
+++ FlashLight/src/main/java/com/jamobox/flashlight/FlashNotification.java
@@ -1,6 +1,5 @@
 package com.jamobox.flashlight;
 
-import android.app.Notification;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.support.v4.app.NotificationCompat;
